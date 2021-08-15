72458fa24c48fb8354ca9ae64073d4dc2d129866
Denis-the <d.rozumnyu@gmail.com>
2021-08-15 22:43:13 +0300

    api key moved to .env file

--- src/api/config.ts
+++ src/api/config.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.0/",
   withCredentials: true,
   headers: {
-    "API-KEY": "5dcf9479-c8ea-474c-9022-737a5cf485ef",
+    "API-KEY": process.env.REACT_APP_API_KEY,
   },
 });
 
